bb9862e3df7b1f6ca14c24576191fceb875dfe3c
Cliffano Subagio <cliffano@gmail.com>
2011-01-16 23:45:47 +1100

    Fixed vows tests structure. Added url.validateCustom .

--- test/unit/data.js
+++ test/unit/data.js
@@ -7,26 +7,26 @@ var dbUrl = 'http://localhost:5984';
 var dbName = 'pranala_test';
 
 vows.describe('Data').addBatch({
-    'Data': {
-        topic: new Data(dbUrl, dbName, true),
-        'create new document': {
-            topic: function(data) {
-	            data.saveUrlDoc('1A', 'http://somelongdomain.org/path1/path2?q=what', '10', new Date(), this.callback, null);
-	        },
-            'has correct properties': function(result, error) {
-		        assert.equal(result._id, '1A');
-		        assert.equal(result.url, 'http://somelongdomain.org/path1/path2?q=what');
-		        assert.equal(result.seq, 10);
-            }
-        },
-        'get inexistent document': {
-            topic: function(data) {
-	            data.getUrlDoc('1X1Y1Z', null, this.callback);
-	        },
-            'has correct properties': function(result, error) {
-                assert.equal(result.error, 'not_found');
-                assert.equal(result.reason, 'missing');
-            }
-        }
+	'data': {
+		topic: new Data(dbUrl, dbName, true),
+		'create new document': {
+			topic: function(data) {
+				data.saveUrlDoc('1A', 'http://somelongdomain.org/path1/path2?q=what', '10', new Date(), this.callback, null);
+			},
+			'has correct properties': function(result, error) {
+				assert.equal(result._id, '1A');
+				assert.equal(result.url, 'http://somelongdomain.org/path1/path2?q=what');
+				assert.equal(result.seq, 10);
+			}
+		},
+		'get inexistent document': {
+			topic: function(data) {
+				data.getUrlDoc('1X1Y1Z', null, this.callback);
+			},
+			'has correct properties': function(result, error) {
+				assert.equal(result.error, 'not_found');
+				assert.equal(result.reason, 'missing');
+			}
+		}
     }
-}).run();
+}).export(module);
--- test/unit/sequence.js
+++ test/unit/sequence.js
@@ -4,30 +4,28 @@ var assert = require('assert'),
     vows = require('vows');
 
 vows.describe('Sequence').addBatch({
-    'Sequence': {
-        'with existing file': {
-            topic: function() {
-	            this.file = 'build/test/sequence-exist.txt';
-	            fs.writeFileSync(this.file, '888');
-	            return new Sequence(this.file);
-	        },
-            'has a value of 0': function(topic) {
-	            assert.equal(topic.get(), 888);
-            },
-            'has a value of 890 after incrementing 2 times': function(topic) {
-	            topic.increment();
-	            topic.increment();
-	            assert.equal(topic.get(), 890);
-            },
-            'has a value of 888 in file': function(topic) {
-	            assert.equal(fs.readFileSync(this.file), 888);
-	            assert.equal(topic.get(), 890);
-            },
-			'has a value of 890 after persisting': function(topic) {
-				topic.persist();
-	            assert.equal(fs.readFileSync(this.file), 890);
-	            assert.equal(topic.get(), 890);
-            }
-        }
-    }
-}).run();
+	'with existing file': {
+		topic: function() {
+			this.file = 'build/test/sequence-exist.txt';
+			fs.writeFileSync(this.file, '888');
+			return new Sequence(this.file);
+		},
+		'has a value of 0': function(topic) {
+			assert.equal(topic.get(), 888);
+		},
+		'has a value of 890 after incrementing 2 times': function(topic) {
+			topic.increment();
+			topic.increment();
+			assert.equal(topic.get(), 890);
+		},
+		'has a value of 888 in file': function(topic) {
+			assert.equal(fs.readFileSync(this.file), 888);
+			assert.equal(topic.get(), 890);
+		},
+		'has a value of 890 after persisting': function(topic) {
+			topic.persist();
+			assert.equal(fs.readFileSync(this.file), 890);
+			assert.equal(topic.get(), 890);
+		}
+	}
+}).export(module);
